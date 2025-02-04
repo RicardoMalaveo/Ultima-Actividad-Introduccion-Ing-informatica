292149be29a912db170eb764bbd0c33f49ce0a7f
IsmaelPerezRodriguez (UNI) <ismaeluniperez@gmail.com>
2025-02-04 17:14:56 +0100

    -Script de movimiento añadido -Movimiento añadido al Le-Cube

---
+++ Assets/Scripts/Movimiento.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    public float velocidad = 5f;
+    public float velocidadRotacion = 100f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    void FixedUpdate()
+    {
+        float movimiento = Input.GetAxis("Vertical") * velocidad;
+        float rotacion = Input.GetAxis("Horizontal") * velocidadRotacion;
+        Vector3 direccion = transform.forward * movimiento;
+        rb.velocity = new Vector3(direccion.x, rb.velocity.y, direccion.z);
+        Quaternion deltaRot = Quaternion.Euler(new Vector3(0, rotacion * Time.fixedDeltaTime, 0));
+        rb.MoveRotation(rb.rotation * deltaRot);
+    }
+}
